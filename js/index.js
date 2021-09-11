3d04e650248c46d58792da11f2b1771dc3cd7042
Greg <gch4rles@gmail.com>
2021-09-11 18:22:33 +0200

    Ajout d'animations sur les scores et le dé

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const scoreLimit = 100;
-const startScore = 0;
+const startScore = 90;
 
 const players = [1, 2]
 var currentPlayer = 0;
@@ -14,16 +14,18 @@ const newGame = () => {
 
   //Set first player as active player
   currentPlayer = 0;
-
+  roundScore = 0;
   //set current visualy active
   $('#player'+players[currentPlayer]).addClass('activePlayer')
   $('#player'+players[1]).removeClass('activePlayer')
+  $('#playerName'+players[currentPlayer]).addClass('redBall')
+  $('#playerName'+players[1]).removeClass('redBall')
   
   //reset players labels
-  $('#currentScorePlayer'+players[0]).innerText = 0;
-  $('#globalScorePlayer'+players[0]).innerText = startScore;  
-  $('#currentScorePlayer'+players[1]).innerText = 0;
-  $('#globalScorePlayer'+players[1]).innerText = startScore;
+  $('#currentScorePlayer'+players[0]).text("0");
+  $('#globalScorePlayer'+players[0]).text(startScore);  
+  $('#currentScorePlayer'+players[1]).text("0");
+  $('#globalScorePlayer'+players[1]).text(startScore);
   
 }
 
@@ -31,13 +33,29 @@ const rollAdice = () => {
   //rand * (max - min) + 1)
   let random = Math.floor(Math.random() * 6) + 1;  
   dice.src = `../images/dice_${random}.png`
+  $("#dice").addClass('dice-bounce')
+  setTimeout(()=> {
+    $("#dice").removeClass('dice-bounce')
+  }, 1000)
+  // $("#dice").removeClass('dice-bounce')
 
+  let currentScorePlayer = $('#currentScorePlayer'+players[currentPlayer]);
   if(random !== 1)
   {    
     roundScore += random;
-    $('#currentScorePlayer'+players[currentPlayer]).text(roundScore);
+    showCounter(currentScorePlayer.text(),roundScore, currentScorePlayer)
+    // currentScorePlayer.text(roundScore);
   }else{    
-    switchToNextPlayer()
+
+    //animation slideDown currentScore (loose)
+    currentScorePlayer.addClass('slide-down')
+    toggleBtn()
+    setTimeout(() => {
+      switchToNextPlayer()
+      currentScorePlayer.removeClass('slide-down')
+      toggleBtn()
+    }, 1200)
+    
   }
   holdBtn.removeAttribute('disabled')
 }
@@ -47,36 +65,42 @@ const switchToNextPlayer = () => {
   //end round for actual player
   $('#currentScorePlayer'+players[currentPlayer]).text(roundScore);
   $('#player'+players[currentPlayer]).toggleClass('activePlayer')
+  $('#playerName'+players[currentPlayer]).toggleClass('redBall')
   //switch to other player
   currentPlayer = currentPlayer === 0 ? 1 : 0;
   //init other player
   $('#player'+players[currentPlayer]).toggleClass('activePlayer')
+  $('#playerName'+players[currentPlayer]).toggleClass('redBall')
 }
 
 const hold = () => {
   holdBtn.setAttribute('disabled', 'disabled')
-
   let currentScore_lbl = $('#currentScorePlayer'+players[currentPlayer])
   let globalScore_lbl = $('#globalScorePlayer'+players[currentPlayer])  
-
+  
+  
+  
   let score = Number(currentScore_lbl.text())
   let playerGlobalScore = Number(globalScore_lbl.text())
   
   let newScore = playerGlobalScore + score
-  globalScore_lbl.text(newScore)
 
+  // globalScore_lbl.text(newScore)
+
+  showCounter(playerGlobalScore, newScore, globalScore_lbl)
+ 
   if(newScore >= scoreLimit)
   {
-    gameOver()
+    gameOver(newScore)
   }else{
     switchToNextPlayer(0)
   }
   
 }
 
-const gameOver = () => {  
+const gameOver = (newScore) => {  
   $(".modal-title").text(`PLAYER ${players[currentPlayer]} IS THE WINNER !!! `)
-  $(".modal-body p").text(`HE WON WITH ${$('#globalScorePlayer'+players[currentPlayer]).text()} POINTS`)
+  $(".modal-body p").text(`HE WON WITH ${newScore} POINTS`)
   $('#myModal').modal('show')
 }
 
@@ -84,13 +108,35 @@ const gameOver = () => {
 //Generate handlers on buttons
 $('#newGameBtn').on('click', newGame )
 $('#rollDiceBtn').on('click', rollAdice)
-$('#holdBtn').on('click', hold)
-
-
-
-//When Jquery is ready, we can load a new game
-$( () => {
-
-  newGame()
+$('#holdBtn').on('click', ()=> {
+  toggleBtn()
+  $('#currentScorePlayer'+players[currentPlayer]).addClass('slide-up')
+  setTimeout(() => {
+    $('#currentScorePlayer'+players[currentPlayer]).removeClass('slide-up')
+    hold()
+    toggleBtn()
+  }, 1200)
 })
 
+
+const toggleBtn = () => {
+  $('#rollDiceBtn').prop('disabled', function(i, v) { return !v; });
+  $('#holdBtn').prop('disabled', function(i, v) { return !v; });
+}
+
+//show 
+const showCounter = (startValue, endValue, targetPlayerField) => {  
+  
+  let interv = setInterval(()=> {
+    if(startValue===endValue)
+    {
+      clearInterval(interv)
+      return
+    }
+    startValue ++ ;
+    targetPlayerField.text(startValue)  
+  },100)
+}
+
+//When Jquery is ready, we can load a new game
+$( () => newGame() )
